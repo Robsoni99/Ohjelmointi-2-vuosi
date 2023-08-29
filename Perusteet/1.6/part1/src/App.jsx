7fe1824fb35e8e1d6560ca156ff7d3dc50340051
RoopeViitala <gr275833@gradia.fi>
2023-08-29 09:25:47 +0300

    Update

--- Perusteet/1.6/part1/src/App.jsx
+++ Perusteet/1.6/part1/src/App.jsx
@@ -1,60 +1,23 @@
+import { useState } from 'react'
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  };
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>Palautteet</h1>
+      <button onClick={() => setGood(good + 1)}>Hyvä</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutraali</button>
+      <button onClick={() => setBad(bad + 1)}>Huono</button>
+
+      <h2>Tilanne:</h2>
+      <p>Hyvä: {good}</p>
+      <p>Neutraali: {neutral}</p>
+      <p>Huono: {bad}</p>
     </div>
-  );
-};
+  )
+}
 
-const Header = ({ course }) => {
-  return (
-    <h1>{course}</h1>
-  );
-};
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part, index) => (
-        <Part key={index} part={part} />
-      ))}
-    </div>
-  );
-};
-
-const Total = ({ parts }) => {
-  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return (
-    <p>Total: {totalExercises}</p>
-  );
-};
-
-export default App;
+export default App
---
+++ Perusteet/1.8/part1/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positivePercentage = (good / total) * 100;
+
+  const css = {
+    margin: 0,
+    padding: 0
+  };
+
+  return (
+    <div id="statistics">
+      <h2>Statistics</h2>
+      <p style={css}>Good: {good}</p>
+      <p style={css}>Neutral: {neutral}</p>
+      <p style={css}>Bad: {bad}</p>
+      <p style={css}>All: {total}</p>
+      <p style={css}>Average: {isNaN(average) ? 0 : average}</p>
+      <p style={css}>
+        Positive: {isNaN(positivePercentage) ? 0 : positivePercentage}%
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <div>
+        <h1>Give Feedback</h1>
+        <button onClick={() => setGood(good + 1)}>Hyvä</button>
+        <button onClick={() => setNeutral(neutral + 1)}>Neutraali</button>
+        <button onClick={() => setBad(bad + 1)}>Huono</button>
+      </div>
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
