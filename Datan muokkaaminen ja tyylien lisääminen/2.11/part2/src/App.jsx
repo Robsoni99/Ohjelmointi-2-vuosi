53c6743a7593ad81ad9c9ca75e10e91ec4f35b4f
RoopeViitala <gr275833@gradia.fi>
2023-10-12 09:36:51 +0300

    update

---
+++ Datan muokkaaminen ja tyylien lisääminen/2.11/part2/src/App.jsx
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Filter = ({ searchTerm, handleSearch }) => {
+  return (
+    <div>
+      filter shown with: <input value={searchTerm} onChange={handleSearch} />
+    </div>
+  );
+};
+
+const PersonForm = ({ newName, newNumber, handleNameChange, handleNumberChange, addPerson }) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        <div>name: <input value={newName} onChange={handleNameChange} /></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = ({ filteredPersons }) => {
+  return (
+    <ul>
+      {filteredPersons.map((person, index) => (
+        <li key={index}>{person.name} {person.number}</li>
+      ))}
+    </ul>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to the phonebook`);
+    } else {
+      const personObject = {
+        name: newName,
+        number: newNumber
+      };
+
+      axios.post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          setPersons(persons.concat(response.data));
+          setNewName('');
+          setNewNumber('');
+        })
+        .catch(error => {
+          console.error('Error adding person:', error);
+        });
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter searchTerm={searchTerm} handleSearch={handleSearch} />
+
+      <h3>Add a new</h3>
+
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        addPerson={addPerson}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons filteredPersons={filteredPersons} />
+    </div>
+  );
+};
+
+export default App;
